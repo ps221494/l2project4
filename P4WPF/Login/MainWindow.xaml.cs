1292e7403d47b41e33fe92e209a38e3321d29cee
Delano van Londen <ps221494@summacollege.nl>
2022-01-27 15:43:31 +0100

    hopelijk

---
+++ P4WPF/Login/MainWindow.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Login.Models;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace Login
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ProjectDB _usersdb = new ProjectDB();
+
+        private Users _Selectedusers;
+
+        public Users Selectedusers
+        {
+            get { return _Selectedusers; }
+            set { _Selectedusers = value; }
+        }
+
+        private ObservableCollection<Users> _LstUsers = new ObservableCollection<Users>();
+
+        public ObservableCollection<Users> LstUsers
+        {
+            get { return _LstUsers; }
+            set { _LstUsers = value; }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateUsers();
+            DataContext = this;
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TxtUserName.Text = "";
+        }
+
+        private void PasswordBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtPassword.Password = "";
+        }
+
+        private void PopulateUsers()
+        {
+            List<Users> UsersfromDB = _usersdb.GetUsers();
+
+            if (UsersfromDB == null)
+            {
+                MessageBox.Show("ERROR");
+            }
+            else
+            {
+                LstUsers.Clear();
+                foreach (Users users in UsersfromDB)
+                {
+                    LstUsers.Add(users);
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Users> users = new List<Users>();
+            users = _usersdb.GetUsers();
+            string name = TxtUserName.Text;
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i].name == name )
+                {
+                    adminwindow win = new adminwindow();
+                    string password; 
+                    string password1 = "$2a$";
+                    string password2;
+                    string password3 = users[i].password;
+                    password2 = password3.Substring(4 , password3.Length-4);
+                    password = password1 + password2;
+
+                    bool Correct = BCrypt.Net.BCrypt.Verify(txtPassword.Password , password);
+                    if (Correct == true)
+                    {
+                        switch (users[i].RoleName)
+                        {
+                            case "management":
+                                MessageBox.Show("manager login");
+                                break;
+                            case "admin":
+                                MessageBox.Show("welkom " + name);
+                                win.Show();
+                                this.Close();
+
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("error");
+                    }
+
+                }
+
+            }
+       //     users = _usersdb.GetUsers();
+          /*  string userName = TxtUserName.Text;
+            string Password = TxtPassword.Password;
+            
+
+            if (!_usersdb.VerifyLogin(userName, Password))
+            {
+                MessageBox.Show("error");
+            }
+            else
+            {
+                MessageBox.Show("gelukt");
+            }*/
+            
+
+
+            //           ````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````````` MessageBox.Show(LstUsers);
+
+
+            //   this.Close();
+            // win.Show();
+
+
+        }
+    }
+}
---
+++ P4WPF/Login/Models/ProjectDB.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;     // Click de System.Configuration aan in References
+using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace Login.Models
+{
+   public class ProjectDB
+    {
+        private MySqlConnection _conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["stonkspizzastring"].ConnectionString);
+
+        public List<Users> GetUsers()
+        {
+            List<Users> resultaat = new List<Users>();
+            try
+            {
+                _conn.Open();
+                MySqlCommand command = _conn.CreateCommand();
+                command.CommandText =
+                    "SELECT users.id, users.name, users.password, users.email, roles.id, roles.rname FROM user_roles INNER JOIN users ON user_roles.user_id = users.id INNER JOIN roles on user_roles.role_id = roles.id";
+                MySqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+
+                foreach (DataRow rij in table.Rows)
+                {
+                    Users users = new Users();
+
+                    users.id = (ulong)rij["id"];
+                    users.name = (string)rij["name"];
+                    users.email = (string)rij["email"];
+                    users.password = (string)rij["password"];
+                    users.RoleName = (string)rij["rname"];
+
+                    Console.ReadLine();
+                    resultaat.Add(users);
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.Error.WriteLine(e.Message);
+            }
+            finally
+            {
+                _conn.Close();
+
+            }
+            return resultaat;
+        }
+
+
+        public bool VerifyLogin(string txtname, string Password)
+        {
+            bool islogged = false;
+            try
+            {
+                _conn.Open();
+                MySqlCommand command = _conn.CreateCommand();
+                command.CommandText =
+                    "SELECT name, email, password "
+                    + "FROM users " +
+                    "WHERE name = @name";
+                command.Parameters.AddWithValue("@name", txtname);
+                command.Parameters.AddWithValue("@password", Password);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    if ((string)reader["name"] == txtname)
+                    {
+                        if (BCrypt.Net.BCrypt.Verify(Password, (string)reader["password"]))
+                        {
+                            islogged = true;
+                        }
+
+                    }
+                    else
+                    {
+                        islogged = false;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                _conn.Close();
+            }
+            return islogged;
+        }
+    }
+}
+
